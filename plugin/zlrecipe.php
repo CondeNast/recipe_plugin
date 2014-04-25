142fabf5401ef86d1f518ca8d38463a3a27a5305
Dave Corboy <DCORBOY@Dave-Corboys-MacBook-Pro.local>
2014-04-25 11:16:45 -0400

    Hide 3PO plugin

--- plugin/zlrecipe.php
+++ plugin/zlrecipe.php
@@ -1432,7 +1432,7 @@ function amd_zlrecipe_format_recipe($recipe) {
 
     $output .= '</div>
           <script type="text/javascript">wk_bootstrap();</script>
-          <img src=\'http://3po.ziplist.com/wp?url=' . urlencode($permalink) . '\' width=\'0\' height=\'0\'>
+          <img src="http://3po.ziplist.com/wp?url=' . urlencode($permalink) . '" style="display: none;" width="0" height="0">
 		</div>';
 
     return $output;
